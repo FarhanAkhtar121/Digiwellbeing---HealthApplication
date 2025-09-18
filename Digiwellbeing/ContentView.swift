47d210ce6de61fe00076266dc966204c85208dbd
FARHAN AKHTAR <aryanakhtar1989@gmail.com>
2025-09-18 14:56:10 +0530

    Initial Commit

---
+++ Digiwellbeing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Digiwellbeing
+//
+//  Created by farhan akhtar on 18/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Digiwellbeing/DigiwellbeingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiwellbeingApp.swift
+//  Digiwellbeing
+//
+//  Created by farhan akhtar on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct DigiwellbeingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
